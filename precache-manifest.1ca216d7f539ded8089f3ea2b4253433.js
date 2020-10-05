e14cb2c0d069f546cf25771de6f89ba6dc29679c
Shubhankar Saxena <saxena03121999@gmail.com>
2020-10-05 19:37:29 +0530

    Updates

---
+++ precache-manifest.1ca216d7f539ded8089f3ea2b4253433.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21b9f3d747470b62b18d89acdd984df",
+    "url": "/Jobs-For-You/index.html"
+  },
+  {
+    "revision": "48fd60f1091b6d83a595",
+    "url": "/Jobs-For-You/static/js/2.8656695d.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/Jobs-For-You/static/js/2.8656695d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a09f837a2b679a22ac9",
+    "url": "/Jobs-For-You/static/js/main.1d284d08.chunk.js"
+  },
+  {
+    "revision": "56bd7be5a4a762c55110",
+    "url": "/Jobs-For-You/static/js/runtime-main.f162eda6.js"
+  }
+]);
